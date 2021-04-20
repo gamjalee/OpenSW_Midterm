e202c8fd0a78b0ca39c634a276271a0dba8c60e6
gamjalee <jfamily0309@gmail.com>
2021-04-20 18:55:37 +0900

    new

--- src/genSnippet.java
+++ src/genSnippet.java
@@ -7,6 +7,7 @@ import java.io.FileOutputStream;
 public class genSnippet {
     public static void main(String[] args) {
         File inputFile = new File("src/input.txt");
+        String [] input = new String [5];
 
     }
 }
