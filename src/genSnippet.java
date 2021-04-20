c5c79545727e4bcdb00d5a28b862489cf7341a6a
gamjalee <jfamily0309@gmail.com>
2021-04-20 18:51:32 +0900

    c

--- src/genSnippet.java
+++ src/genSnippet.java
@@ -1,2 +1,12 @@
+//intelliJ 를 사용하여 작성한 코드입니다.
+//202012316 이지영
+
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class genSnippet {
+    public static void main(String[] args) {
+        File inputFile = new File("src/input.txt");
+
+    }
 }
